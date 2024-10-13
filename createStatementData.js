1a092a49e9a770aee17cdedb1dfcc06c16ab6260
hsbeencom-fanmaum <140990483+hsbeencom-fanmaum@users.noreply.github.com>
2024-10-14 00:47:19 +0900

    타입 코드를 서브클래스로 바꾸기 - createPerformanceCalculator

--- createStatementData.js
+++ createStatementData.js
@@ -32,7 +32,14 @@ function createStatementData(invoice, plays) {
 }
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay);
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
 }
 
 class PerformanceCalculator {
@@ -71,5 +78,9 @@ class PerformanceCalculator {
     return result;
   }
 }
+
+class TragedyCalculator extends PerformanceCalculator {}
+
+class ComedyCalculator extends PerformanceCalculator {}
 
 module.exports = { createStatementData };
